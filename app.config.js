a56b4824dc764c8cc81b2f6659b7b783c3159922
migueldhyne <87825608+migueldhyne@users.noreply.github.com>
2026-06-09 10:43:06 +0200

    Create app.config.js

---
+++ app.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'Déclic IA',
+  slug: 'declic-ia-pocket',
+  version: '1.15.2',
+  owner: 'migueldhyne2',
+  android: {
+    package: 'net.generationdeclic.declicia'
+  },
+  extra: {
+    eas: {
+      projectId: '939d9503-57ea-4029-b55f-e50392011f7a'
+    }
+  }
+};
