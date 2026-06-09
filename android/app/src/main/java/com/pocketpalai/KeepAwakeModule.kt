9e4e2f8c209cf38066cae519366886c7f975e86f
migueldhyne <87825608+migueldhyne@users.noreply.github.com>
2026-06-09 15:20:59 +0200

    Update KeepAwakeModule.kt

--- android/app/src/main/java/com/pocketpalai/KeepAwakeModule.kt
+++ android/app/src/main/java/com/pocketpalai/KeepAwakeModule.kt
@@ -3,7 +3,7 @@ package com.pocketpalai
 import android.view.WindowManager
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.annotations.ReactModule
-import com.pocketpal.specs.NativeKeepAwakeSpec
+import com.pocketpalai.specs.NativeKeepAwakeSpec
 
 @ReactModule(name = NativeKeepAwakeSpec.NAME)
 class KeepAwakeModule(reactContext: ReactApplicationContext) :
