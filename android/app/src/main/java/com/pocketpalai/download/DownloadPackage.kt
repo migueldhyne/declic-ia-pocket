a350fa7a86ba92a9f3ada7914f7c310645d238a2
migueldhyne <87825608+migueldhyne@users.noreply.github.com>
2026-06-09 15:22:02 +0200

    Update DownloadPackage.kt

--- android/app/src/main/java/com/pocketpalai/download/DownloadPackage.kt
+++ android/app/src/main/java/com/pocketpalai/download/DownloadPackage.kt
@@ -5,7 +5,7 @@ import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfo
 import com.facebook.react.module.model.ReactModuleInfoProvider
-import com.pocketpal.specs.NativeDownloadModuleSpec
+import com.pocketpalai.specs.NativeDownloadModuleSpec
 
 class DownloadPackage : TurboReactPackage() {
   override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
